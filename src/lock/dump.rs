05b8c9567ddc4e23169b5e4f22bbaf882a6dc537
Dimitri Merejkowsky <dimitri.merejkowsky@tanker.io>
2019-10-23 12:00:30 +0200

    Remove outdated `allow(clippy)` annotation

--- src/lock/dump.rs
+++ src/lock/dump.rs
@@ -7,7 +7,6 @@ where
     // Dependencies are sorted according to their *lowercase* name.
     // This is consistent with how `pip freeze` is implemented.
     // See bottom of pip/_internal/operations/freeze.py:freeze()
-    #![allow(clippy::redundant_closure)]
     let mut lines: Vec<_> = locked_dependencies.into_iter().map(|x| x.line()).collect();
     lines.sort_by(|x, y| x.to_lowercase().cmp(&y.to_lowercase()));
     let mut res = lines.join("");
