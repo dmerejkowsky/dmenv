e1d72b1c8ec563561596a65c787f2c9c231a598a
Dimitri Merejkowsky <dimitri.merejkowsky@tanker.io>
2019-10-23 17:54:08 +0200

    spectral_helpers: add is_absent_from and is_present_in matchers

--- spectral_helpers/src/lib.rs
+++ spectral_helpers/src/lib.rs
@@ -1,7 +1,27 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use spectral::Spec;
+
+pub trait IsAbsentFrom {
+    fn is_absent_from(&mut self, text: &str);
+}
+
+impl<'s> IsAbsentFrom for Spec<'s, &str> {
+    fn is_absent_from(&mut self, text: &str) {
+        let subject = self.subject;
+        if text.contains(subject) {
+            panic!("\n{}\nshould be absent from\n{}", subject, text);
+        }
+    }
+}
+
+pub trait IsPresentIn {
+    fn is_present_in(&mut self, text: &str);
+}
+
+impl<'s> IsPresentIn for Spec<'s, &str> {
+    fn is_present_in(&mut self, text: &str) {
+        let subject = self.subject;
+        if !text.contains(subject) {
+            panic!("\n{}\nshould be present in\n{}", subject, text);
+        }
     }
 }
