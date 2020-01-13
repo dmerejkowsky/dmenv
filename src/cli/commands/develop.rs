ee4279de1361766a3704a43be121aafb513bad0b
Dimitri Merejkowsky <d.merej@gmail.com>
2020-01-13 22:01:08 +0100

    Fix error message when calling `dmenv develop` without virtual env

--- src/cli/commands/develop.rs
+++ src/cli/commands/develop.rs
@@ -1,3 +1,4 @@
+use crate::cli::commands;
 use crate::error::*;
 use crate::ui::*;
 use crate::Context;
@@ -8,6 +9,7 @@ pub fn develop(context: &Context) -> Result<(), Error> {
     let Context {
         paths, venv_runner, ..
     } = context;
+    commands::expect_venv(&context)?;
     print_info_2("Running setup_py.py develop");
     if !&paths.setup_py.exists() {
         return Err(Error::MissingSetupPy {});
